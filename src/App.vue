e5e2817fc880588371e3eed96fcd4fca30e0c00e
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2021-07-18 05:28:51 -0300

    Adicionando progress bar
    Implementando barra de progresso  e utilizando fileReader.

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,28 @@
 
       <TarefasLista />
 
+      <button 
+        class="btn btn-primary mt-4 mb-2"
+        @click="download">
+          Baixar Imagem
+      </button>
+
+      <div class="progress">
+        <div 
+          class="progress-bar" 
+          role="progressbar" 
+          :style="{ width: progresso + '%' }" 
+          :aria-valuenow="progresso" 
+          aria-valuemin="0" 
+          aria-valuemax="100">
+            {{ progresso }} %
+        </div>
+      </div>
+
+      <div v-if="imagem">
+        <img :src="imagem" style="max-width: 100%">
+      </div>
+
     </div>
 
   </div>
@@ -28,10 +50,14 @@ export default {
     components: {
         TarefasLista
     },
-
-    // Requisições simultâneas 
-  async  created(){
-    /*  axios.all([
+    data() {
+      return {
+        progresso: 0,
+        imagem: undefined
+      }
+    },
+    async created() {
+      /*axios.all([
         axios.get(`${config.apiURL}/tarefas/1`),
         axios.get(`${config.apiURL}/tarefas/3`)
       ]).then(axios.spread((tarefa1, tarefa3) => {
@@ -40,20 +66,47 @@ export default {
         console.log('Tarefa 3: ', tarefa3)
       }))*/
 
-      //Requisições simultâneas sem o operador Spread
-     /* axios.all([
+      /*axios.all([
         axios.get(`${config.apiURL}/tarefas/1`),
         axios.get(`${config.apiURL}/tarefas/3`)
       ]).then(response => {
-        const [tarefa1,  tarefa3] = response
-        console.log('Tarefa 1: ', tarefa1)
-        console.log('Tarefa 3: ', tarefa3)
-      })*/
-      const tarefa1 = await  axios.get(`${config.apiURL}/tarefas/1`)
-      const tarefa3 = await  axios.get(`${config.apiURL}/tarefas/3`)
+        const [tarefa1, tarefa3] = response
         console.log('Requisições simultâneas:')
         console.log('Tarefa 1: ', tarefa1)
         console.log('Tarefa 3: ', tarefa3)
+      })*/
+
+      const tarefa1 = await axios.get(`${config.apiURL}/tarefas/1`)
+      const tarefa3 = await axios.get(`${config.apiURL}/tarefas/3`)
+
+      console.log('Requisições simultâneas:')
+      console.log('Tarefa 1: ', tarefa1)
+      console.log('Tarefa 3: ', tarefa3)
+
+    },
+    methods: {
+      download(event) {
+        axios.get(
+          'https://images.unsplash.com/photo-1517976487492-5750f3195933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+          {
+            responseType: 'blob',
+            onDownloadProgress: (progressEvent) => {
+              console.log('Fazendo download...', progressEvent)
+              this.progresso = (progressEvent.loaded / progressEvent.total * 100).toFixed(0)
+            }
+          }
+        ).then(response => {
+
+          console.log('Download concluído!', response)
+
+          const reader = new window.FileReader()
+          reader.readAsDataURL(response.data)
+          reader.onload = () => {
+            this.imagem = reader.result
+          }
+
+        })
+      }
     }
 }
 </script>
